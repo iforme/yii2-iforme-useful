a6a2107c3f78c22eb0eb867522dab09b0687c5a1
Rodrigo de Brum Chimainski <rodrigo@iforme.com.br>
2015-12-08 20:25:00 -0200

    method CEP format

--- Format.php
+++ Format.php
@@ -12,15 +12,27 @@ class Format
      * @param string $phone
      * @return string
      */
-    public static function CellPhone($phone)
+    public static function Phone($phone)
     {
         $phone = Regex::OnlyInteger($phone);
         if(strlen($phone) == 10){
             $phone = '(' . substr($phone, 0, 2) . ') ' . substr($phone, 2, 4) . '-' . substr($phone, 6, 4);
-        }else{
+        } else {
             $phone = '(' . substr($phone, 0, 2) . ') ' . substr($phone, 2, 5) . '-' . substr($phone, 7, 4);
         }
         return $phone;
+    }
+
+    /**
+     * Return integers values in format 99999-999
+     * @param string $cep
+     * @return string
+     */
+    public static function CEP($cep)
+    {
+        $cep = Regex::OnlyInteger($cep);
+        $cep = substr($cep, 0, 5) . '-' . substr($cep, 5, 3);
+        return $cep;
     }
 
     /**
