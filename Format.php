1047c842260568235789d7acca781c811eb3a9e7
Rodrigo de Brum Chimainski <rodrigo@iforme.com.br>
2015-12-08 20:13:37 -0200

    Class Format to display

---
+++ Format.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace iforme\useful;
+
+/**
+ * This is just an example.
+ */
+class Format
+{
+    /**
+     * Return integers values in format (99) 99999-9999
+     * @param string $phone
+     * @return string
+     */
+    public static function CellPhone($phone)
+    {
+        $phone = Regex::OnlyInteger($phone);
+        if(strlen($phone) == 10){
+            $phone = '(' . substr($phone, 0, 2) . ') ' . substr($phone, 2, 4) . '-' . substr($phone, 6, 4);
+        }else{
+            $phone = '(' . substr($phone, 0, 2) . ') ' . substr($phone, 2, 5) . '-' . substr($phone, 7, 4);
+        }
+        return $phone;
+    }
+
+    /**
+     * Return integers values in format 999.999.999-99
+     * @param string $cpf
+     * @return string
+     */
+    public static function CPF($cpf)
+    {
+        $cpf = Regex::OnlyInteger($cpf);
+        $cpf = substr($cpf, 0, 3) . '.' . substr($cpf, 3, 3) .
+                '.' . substr($cpf, 6, 3) . '-' . substr($cpf, 9, 2);
+        return $cpf;
+    }
+}
